0d70be3dc0c9d5e8f728751d9d75db9c3cc5e35e
Lewis Sneddon <>
2021-02-25 12:42:30 +0000

    Toggle to functional component and useState

--- src/components/Windows/Toggle.jsx
+++ src/components/Windows/Toggle.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Toggle = () => {
+
+  const [ conversionSystem, setConversionSystem ] = useState("unselected");
+
   return (
     <div className="window-container">
-      <p>Which system are we converting from?</p>
-      <button>Imperial</button>
+      <p>Which system are we converting from? {conversionSystem}</p>
+      <button
+        onClick={() => setConversionSystem("Imperial")}
+      >Imperial</button>
       <p className="subheading">or</p>
-      <button>US Legal</button>
+      <button
+        onClick={() => setConversionSystem("US Legal")}
+      >US Legal</button>
     </div>
   );
 };
